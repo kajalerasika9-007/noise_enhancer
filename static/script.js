067c1466ec8a253114baa80d7723b6a16e7195a9
kajalerasika9-007 <kajalerasika9@gmail.com>
2026-06-05 13:21:41 +0530

    Update processVideo function to use new API URL

--- static/script.js
+++ static/script.js
@@ -12,7 +12,7 @@ async function processVideo() {
   link.hidden = true;
 
   try {
-    const res = await fetch('/process', {
+    const res = await fetch('https://noise-enhancer-6.onrender.com/process', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ url })
